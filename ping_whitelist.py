bad3b2f62eac95df0bbbeefc729c704848236ef6
dubkooooooooooooov <grizniy1530@proton.me>
2025-12-16 23:09:45 +0300

    improved ping

--- ping_whitelist.py
+++ ping_whitelist.py
@@ -1,33 +1,37 @@
-from ping3 import ping
-from urllib.parse import urlparse
-
-def extract_host(data: str) -> str | None:
-    try:
-        parsed = urlparse(data)
-        return parsed.hostname
-    except Exception:
-        return None
+from singbox2proxy import SingBoxProxy
 
 def test_ping(host):
     try:
-        response = ping(host, timeout=0.5)
-        if response is not None:
-            print(f"Ping to {host} successful, round-trip time: {response} ms")
+        response = host.request("GET", "https://api.ipify.org?format=json", timeout=1, retries=1)
+        if response.status_code == 200:
+            print(f"Ping to {response.text} successful, round-trip time: {response} ms")
             return True
         else:
-            print(f"Ping to {host} failed")
+            print(f"Ping to {response.text} failed")
             return False
     except Exception:
-        print(f"Ping to {host} failed")
+        print(f"Ping failed")
+        return False
 
 file = open("whitelist.txt", "r")
 lines = file.readlines()
+file.close()
 
 out_file = open("working_vpn.txt", "w")
 
+c = 0
 for line in lines:
+    c += 1
     line = line.strip()
-    ip_address = extract_host(line)
-    test = test_ping(ip_address)
-    if test == True:
-        out_file.write(line + "\n")
+    try:
+        with SingBoxProxy(line) as vless_address:
+            test = test_ping(vless_address)
+            print(f"\nProgress: {c}/{len(lines)}")
+            if test == True:
+                out_file.write(line + "\n")
+                out_file.flush()
+    except Exception as e:
+        print(f"Error creating proxy object for line {c}: {e}")
+        continue
+
+out_file.close()
